aa031c6b8fc268ebb344ff6743e6c3e824b8f477
Shreyansh-Verma <58723872+Shreyansh-Verma@users.noreply.github.com>
2020-01-04 13:49:57 +0530

    New line

--- hellow.c
+++ hellow.c
@@ -3,6 +3,7 @@ int main ()
 {
 	pritnf ("Hello New World\n");
 	pritnf ("Hello New World\n");
+	printf ("Happy New year")
 	pritnf ("Hello New World2\n");
 	printf ("Hello New World3\n");
 	printf ("Hello New World4\n");
