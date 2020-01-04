0f81a1876f22ef533289bbe54f1d66bb73c1c7f0
Shreyansh-Verma <shreyansh.verma245@gmail.com>
2020-01-04 13:36:23 +0530

    error check1

--- hellow.c
+++ hellow.c
@@ -3,7 +3,7 @@ int main ()
 {
 	pritnf ("Hello New World\n");
 	pritnf ("Hello New World\n");
-	printf ("Bhag dk bose\n");
+	printf ("Bhag dk bose1\n");
 	pritnf ("Hello New World2\n");
 	printf ("Hello New World3\n");
 	printf ("Hello New World4\n");
